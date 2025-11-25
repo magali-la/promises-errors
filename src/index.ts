fce1d92dc7fb2b51929797dba58c60ee57bb4ac5
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-24 21:23:28 -0500

    feat(displayData): added nested for of loop to find reviews based on product id, added catch method to log error from rejected promise

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,31 @@ import { fetchProductCatalog, fetchProductReviews, fetchSalesReport } from "./ap
 const displayData = () => {
     // fetch the product catalog and store it as an array of objects
     fetchProductCatalog()
+        // once the promise is settled, take it in as the products List
         .then((productsList) => {
-            let currentProducts = productsList;
-            console.log("Fetched product catalog: ", currentProducts);
+            console.log("Fetched product catalog: ", productsList);
+            return productsList;
+        })
+        .then((productsList) => {
+            // then loop through current products and find reviews using the fetch review function
+            for (let product of productsList) {
+                // store the product ID, then run and console log the reviews that match
+                let productID = product.id;
+
+                // call the fetch reviews function with productID variable
+                fetchProductReviews(productID)
+                    // set a then to return the review for the product
+                    .then((reviews) => {
+                        console.log(`Product review found for product ID ${productID}:`, reviews);
+                    })
+                    .catch((error) => {
+                        console.error("Error found while fetching product review: ", error);
+                    });
+            }  
         })
         .catch((error) => {
-            console.error("Error found: ", error);
+            console.error("Error found while fetching product catalog: ", error);
         })
-}
+};
 
 displayData();
-
-// displayData(1);
-// displayData(2);
-// // error state - no matching product or reviews
-// displayData(3);
